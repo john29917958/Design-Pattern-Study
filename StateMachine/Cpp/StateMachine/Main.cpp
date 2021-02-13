2c5d134aaac0f16f611b87df9afa315eb30b980a
Tzu-Chao, Wang <john29917958@gmail.com>
2021-02-13 21:24:15 +0800

    Adds basic scene transition related codes

--- StateMachine/Cpp/StateMachine/Main.cpp
+++ StateMachine/Cpp/StateMachine/Main.cpp
@@ -7,4 +7,10 @@ int main()
 	scene_controller* controller = new scene_controller();
 	scene* scene = new start_scene(controller);
 	controller->set_scene(scene);
+
+	scene = new menu_scene(controller);
+	controller->set_scene(scene, true);
+
+	scene = new battle_scene(controller);
+	controller->set_scene(scene, true);
 }
