be1a7debb82523f0119438f166468f8156d7a365
Tzu-Chao, Wang <john29917958@gmail.com>
2021-02-27 11:59:09 +0800

    Adds C# version of bridge pattern

---
+++ Bridge/CSharp/Bridge/Bridge/Characters/Orc.cs
@@ -0,0 +1,18 @@
+﻿namespace Bridge.Characters
+{
+    public class Orc : Character
+    {
+        public Orc(int id, string name, int initHealth, int initMana) : base(id, name, initHealth, initMana)
+        {
+
+        }
+
+        public override void TakeDamage(int value)
+        {
+            value -= 3;
+            if (value <= 0) value = 0;
+
+            base.TakeDamage(value);
+        }
+    }
+}
