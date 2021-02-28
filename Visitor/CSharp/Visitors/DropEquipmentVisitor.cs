609396d1bfd28032cf350b4780f9ba940e359501
Tzu-Chao, Wang <john29917958@gmail.com>
2021-02-28 17:56:37 +0800

    Adds C# version visitor

---
+++ Visitor/CSharp/Visitors/DropEquipmentVisitor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Visitor.Equipments;
+
+namespace Visitor.Visitors
+{
+    public class DropEquipmentVisitor : EquipmentVisitor
+    {
+        private readonly Equipment _equipment;
+        private readonly Backpack _backpack;
+
+        public DropEquipmentVisitor(Equipment equipment, Backpack backpack)
+        {
+            _equipment = equipment;
+            _backpack = backpack;
+        }
+
+        public override void VisitEquipment(Equipment equipment)
+        {
+            if (equipment == _equipment)
+            {
+                Console.WriteLine($"Drops equipment \"{equipment.Name}\".");
+                _backpack.Remove(equipment);
+                GameSystem.Instantiate(equipment);
+            }
+        }
+
+        public override void VisitArmor(Armor armor)
+        {
+            VisitEquipment(armor);
+        }
+
+        public override void VisitWeapon(Weapon weapon)
+        {
+            VisitEquipment(weapon);
+        }
+    }
+}
