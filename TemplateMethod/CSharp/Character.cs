42820ade9bd893257d417e580aae8b3b15ab91d3
Tzu-Chao, Wang <john29917958@gmail.com>
2021-03-03 20:12:53 +0800

    Adds template method implementation in C#

---
+++ TemplateMethod/CSharp/Character.cs
@@ -0,0 +1,9 @@
+﻿namespace TemplateMethod
+{
+    public class Character
+    {
+        public Vector Position { get; set; }
+        public int Health { get; protected set; }
+        public int Mana { get; protected set; }
+    }
+}
---
+++ TemplateMethod/CSharp/Spell.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using TemplateMethod.SpellCastingValidators;
+
+namespace TemplateMethod
+{
+    public class Spell
+    {
+        public float CoolDownCounter { get; protected set; }
+
+        public SpellCastingValidator Validator
+        {
+            get { return _validator; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    _validator = value;
+                }
+            }
+        }
+
+        private SpellCastingValidator _validator;
+
+        public void Cast()
+        {
+            if (Validator == null || Validator.IsCastable())
+            {
+                Console.WriteLine("Casts spell");
+            }
+        }
+    }
+}
